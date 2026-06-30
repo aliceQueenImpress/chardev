8f2385a73f95f9251fbf0ed6ca6e3796531fe715
alice <alice@vbox.myguest.virtualbox.org>
2026-06-30 20:40:31 +0000

    feat: add default values on init/reset

--- chardev.h
+++ chardev.h
@@ -3,6 +3,11 @@
 
 #include <linux/types.h>
 
+//default device values
+#define DEFLT_SPEED        20
+#define DEFLT_AUTO_MODE    1
+#define DEFLT_PROFILE      "default"
+
 struct fan_chardev_struct {
     int32_t speed; //percentage of the speed (0-100)
     int32_t auto_mode; //auto_matic mode 1 = auto, 0 = manual
